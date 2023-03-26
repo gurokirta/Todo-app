ccb052b0b798e8e37ed2690e37eff1da0cad4e89
Guro <70147494+gurokirta@users.noreply.github.com>
2023-03-26 14:33:47 +0400

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import Todo from "./Components/todo";
 import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <Todo />
